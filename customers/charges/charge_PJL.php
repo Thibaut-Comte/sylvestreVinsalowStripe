a5dee5548e92e8ea63803e46ab9dfd8ec415ba4f
Thibaut Comte <thibaut.comte.1@gmail.com>
2018-08-23 17:14:17 +0200

    innitial commit

---
+++ customers/charges/charge_PJL.php
@@ -0,0 +1,69 @@
+<?php 
+
+    $total = $_POST['total'];
+
+    $token = $_POST['stripeToken'];
+
+    //Moins ou égale à d'1€ aucune charge n'est appliquée
+    if ($total <= 100) {
+		exit(0);
+	} //fin condition.
+
+    require_once('../../vendor/autoload.php');
+    include_once('../keys.php');
+
+    //A modifier si nécessaire dans le fichier keys.php
+    \Stripe\Stripe::setApiKey($STRIPE_SECRET_KEY_PJL);
+
+    //Compte qui reçoit l'argent
+    $stripeAccountId = 'acct_1AzQlhCWCYSXjXda';
+
+    //Modifier les nombres en fonction du du client
+	$brandRight = $total * 0.00;
+	$authorRight = $total * 0.00;
+	$bankFees = $total * 0.045;
+	$salesFees = $total * 0.00;
+	$marketingFees = $total * 0.00;
+	$brandFees = $total * 0.00;
+	$authorFees = $total * 0.00;
+	$billFees = $total * 0.00;
+
+
+	//Total des charges de la commission
+	$totalFees = $brandRight + $authorRight + $bankFees + $salesFees + $marketingFees + $brandFees + $authorFees + $billFees;
+
+	
+	//Si le montant de la commission est inférieur à 50 centimes, le passe à 50 centimes
+	if ($totalFees < 50) {
+		$totalFees = 50;
+	}
+	else if ($totalFees > 50){
+		$totalFees = $totalFees;
+	}
+	
+
+	//Montant du total moins les charges
+	$amount = $total - $totalFees;
+
+  try {
+    $charge = \Stripe\Charge::create(array(
+      "amount" => $amount,
+      "currency" => "eur",
+      "source" => $token,
+      "destination" => array(
+        "amount" => $totalFees,
+        "account" => "$stripeAccountId",
+      ),
+    ));
+  }
+  catch (Exception $e){
+    echo ("Une erreur est survenue sur notre site, contactez le SAV.");
+  }
+    
+
+      //Redirection après paiement réussi
+    header('Location: https://semiremorques.pro/');
+          
+  exit(0);
+
+?>
---
+++ customers/keys.php
@@ -0,0 +1,25 @@
+<?php
+	//Id du receveur officiel des paiements Bassique Secret API id (celles des clients)
+	$STRIPE_SECRET_KEY_LBA = 'sk_live_subrq1pWZufGTnbtTAFaaHvP';
+
+	$STRIPE_SECRET_KEY_BATR = 'sk_live_cqYONpi0WkQ6M5ki5qkS1qiL';
+
+	$STRIPE_SECRET_KEY_JTS = 'sk_live_TTQzdkjhU64bWBsAEGbzPGvY';
+
+	$STRIPE_SECRET_KEY_SETUP = 'sk_live_V8WQ7XuRO9q65L55w7l8yyyA';
+
+	$STRIPE_SECRET_KEY_AUVERGNE_PROMOS = 'sk_live_VFAKgPuWKbshrCQoQDYS64L4';
+
+	$STRIPE_SECRET_KEY_KINE = '';
+
+	$STRIPE_SECRET_KEY_MOOV = 'sk_live_pSTrwCftxGai7mHYPrIfxUT2';
+
+	$STRIPE_SECRET_KEY_AKO = 'sk_live_tLon8xNFbzQZv3oYAdDPCW4H';
+
+	$STRIPE_SECRET_KEY_PJL = 'sk_live_QAuxRFVuH9NWrSCsUC8AaDmy';
+
+	$STRIPE_SECRET_KEY_VINSALOW = 'sk_live_lGwyHzYTVZs5gbK8MZXkVE7w';
+
+
+
+?>
